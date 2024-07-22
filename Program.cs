f258f6359a12ac0d58f5b3cec702798228f7ee06
wulitina <tinamazing.yan.xu@gmail.com>
2024-07-23 00:00:25 +0100

    first commit for c# calculator

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Calculater {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, welcome to the calculator program");
+            Console.ReadKey();
+        }
+    }
+}
